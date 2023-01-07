7c9b3f8198815a0ff602ed598e8ad19fd7d19990
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-01-07 12:52:44 -0800

    Added forEach loop to style the page

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-let _calendar = $("#calendar");
+let _calendar = $("#calendar");                 //Tie in whole calendar div
+let _hours = [..._calendar.children()];              //Get a list of hours divs from the calendar
+let todayInHours = dayjs().format("HH");
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -17,20 +19,30 @@ $(function () {
       
     }
   })
-
-console.log(_calendar);
-      console.dir(_calendar.children().children());
-
+  
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
+  _hours.forEach(hour => {
+    if(hour.id.slice(-2) < todayInHours){
+      $(hour).addClass("past");
+    } else if(hour.id.slice(-2) == todayInHours){
+      $(hour).addClass("present");
+    } else {
+      $(hour).addClass("future");
+    }
+  });
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+
 });
