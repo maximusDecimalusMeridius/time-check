61273e8fc2d54f9f142b801547abce6458a047ae
Andy Gaudy <andygaudy@MacBook-Pro.local>
2023-01-07 16:47:27 -0800

    Added comments and cleaned up formatting of code

--- Develop/script.js
+++ Develop/script.js
@@ -1,68 +1,72 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  let _calendar = $("#calendar");                      //Tie in whole calendar div
-  let _hours = [..._calendar.children()];              //Get a list of hours divs from the calendar
-  let todayInHours = dayjs().format("HH");             //set var to HH format of current hour of dayjs on page load
-  let thingsToDo = [];
-  
-  //Create newAppointment object and constructor
-  class appointment{
-    constructor(id){
-      this.id = id;
-      this.details = "";
-    }
-  }
+let _calendar = $("#calendar");                      //Tie in whole calendar div
+let _hours = [..._calendar.children()];              //Get a list of hours divs from the calendar
+let todayInHours = dayjs().format("HH");             //set var to HH format of current hour of dayjs on page load
+let thingsToDo = [];                                 //Array that will hold all meeting objects (id, details) during user session
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-  _calendar.on("click", (event) => {
-    let target = $(event.target);
-    if(target.hasClass("saveBtn")){
-      target.prev()[0].value = target.prev()[0].value.trim();
-      console.log(thingsToDo);
-    }
-  })
-  
-  //Add styling to hour blocks based on time relative to current time when page is loaded
-  function checkTime(){
-  _hours.forEach(hour => {
-    if(hour.id.slice(-2) < todayInHours){
-      $(hour).addClass("past");
-    } else if(hour.id.slice(-2) == todayInHours){
-      $(hour).addClass("present");
-    } else {
-      $(hour).addClass("future");
-    }
-  });
+//Create appointment object and constructor
+class appointment{
+  constructor(id){
+    this.id = id;
+    this.details = "";
   }
+}
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements.
-  
-  if(localStorage.getItem("thingsToDo") == null){
-    localStorage.setItem("thingsToDo", "");
+//Event listener for save button.  On save, trim() input and save it to the calendar
+_calendar.on("click", (event) => {
+  let target = $(event.target);
+  if(target.hasClass("saveBtn")){
+    
+    for(let i = 0; i < _hours.length; i++){
+      if(_hours[i].id == target.parent()[0].id){
+        target.prev()[0].value = target.prev()[0].value.trim();
+        thingsToDo[i].details = target.prev()[0].value;
+        localStorage.setItem("thingsToDo", JSON.stringify(thingsToDo));
+      }
+    }
+
   }
-  
-  if(JSON.parse(localStorage.getItem("thingsToDo") == "")){
-      //populate thingsToDo with objects and IDs of corresponding elements
-      for(let i = 0; i < _hours.length; i++){
-        let appointmentInfo = new appointment(_hours[i].id);
-        thingsToDo.push(appointmentInfo);
-        console.log(thingsToDo);
-      } 
+})
+
+//Add styling to hour blocks based on time relative to current time when page is loaded
+function checkTime(){
+_hours.forEach(hour => {
+  if(hour.id.slice(-2) < todayInHours){
+    $(hour).addClass("past");
+  } else if(hour.id.slice(-2) == todayInHours){
+    $(hour).addClass("present");
   } else {
-      thingsToDo = JSON.parse(localStorage.getItem("thingsToDo"));
+    $(hour).addClass("future");
   }
+});
+}
 
-  // TODO: Add code to display the current date in the header of the page.
+//Populate the tracker on load
+function populateTracker() {
+for(let i = 0; i < thingsToDo.length; i++){
+  _calendar.children().eq(i).children("textarea")[0].value = thingsToDo[i].details;
+}
+}
 
-  checkTime();
+//Initialize local storage if it doesn't exist
+if(localStorage.getItem("thingsToDo") == null){
+  localStorage.setItem("thingsToDo", "");
+}
 
+//If localStorage doesn't exist, perform initialize of object array thingsToDo
+//If localStorage does exist, copy values into thingsToDo array on load
+if(JSON.parse(localStorage.getItem("thingsToDo") == "")){
+    //populate thingsToDo with objects and IDs of corresponding elements
+    for(let i = 0; i < _hours.length; i++){
+      let appointmentInfo = new appointment(_hours[i].id);
+      thingsToDo.push(appointmentInfo);
+    } 
+} else {
+    thingsToDo = JSON.parse(localStorage.getItem("thingsToDo"));
+}
+
+//Page setup on load
+
+checkTime();                    //Check current time and apply appropriate styling
+populateTracker();              //Populate tracker with existing data
 });
